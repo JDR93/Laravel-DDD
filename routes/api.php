713e0a3b83048908a46b0b54b0c9ee2cbaf3303d
JDR93 <julian310127@gmail.com>
2026-01-07 14:56:38 -0500

    Change prefix of command DDDStructrure for '_' to '/'

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::prefix('landing_user')->group(base_path('src/landing/user/infrastructure/routes/api.php'));
-
-Route::prefix('purchase_user')->group(base_path('src/purchase/user/infrastructure/routes/api.php'));
